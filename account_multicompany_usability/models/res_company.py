1d3f431c1355252296bf3215b90706aaff380d29
Katherine Zaoral <kz@adhoc.com.ar>
2018-05-24 18:43:21 -0300

    squash! [FIX] account_multicompany_usability: Models
    - remove not needed imports
    - remove not used loggers
    - remove string from field definition when name of field = string
    - convert code comments into method docstrings.

--- account_multicompany_usability/models/res_company.py
+++ account_multicompany_usability/models/res_company.py
@@ -3,9 +3,6 @@
 # directory
 ##############################################################################
 from odoo import fields, models, api
-import logging
-
-_logger = logging.getLogger(__name__)
 
 
 class ResCompany(models.Model):
@@ -16,18 +13,17 @@ class ResCompany(models.Model):
         help='Short name used to identify this company',
     )
 
+    consolidation_company = fields.Boolean(
+    )
+
     @api.multi
     def get_company_sufix(self):
-        # cuando pedimos para unr registro que no tiene sia no queremos
-        # que ensure_one arroje error
-        if (
-                len(self) != 1 or
-                self._context.get('no_company_sufix') or
-                not self.env.user.has_group('base.group_multi_company')):
-            return ''
-        else:
-            return ' (%s)' % (self.short_name or self.name)
+        """ Cuando pedimos para unr registro que no tiene sia no queremos que
+        ensure_one arroje error
+        """
+        condition =  \
+            len(self) != 1 or \
+            self._context.get('no_company_sufix') or \
+            not self.env.user.has_group('base.group_multi_company')
 
-    consolidation_company = fields.Boolean(
-        'Consolidation Company',
-    )
+        return '' if condition else '(%s)' % (self.short_name or self.name)
