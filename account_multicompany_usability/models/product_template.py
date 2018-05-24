06dddba6353e02bc157a83fb6b681bd79faea0a5
Katherine Zaoral <kz@adhoc.com.ar>
2018-05-24 18:39:00 -0300

    fixup! [FIX] account_multicompany_usability: Models

--- account_multicompany_usability/models/product_template.py
+++ account_multicompany_usability/models/product_template.py
@@ -17,23 +17,35 @@ class ProductTemplate(models.Model):
         'res.company.property',
         string="Income Account",
         context=_get_property_context('property_account_income_id'),
-        compute='_get_properties',
+        compute='_compute_properties',
     )
     property_account_expense_ids = fields.Many2many(
         'res.company.property',
         string="Expense Account",
         context=_get_property_context('property_account_expense_id'),
-        compute='_get_properties',
+        compute='_compute_properties',
     )
 
-    @api.one
-    def _get_properties(self):
-        company_props = self.env['res.company.property'].with_context(
-            active_model='product.template', active_id=self.id)
-        self.property_account_income_ids = company_props.with_context(
-            property_field='property_account_income_id')._get_companies()
-        self.property_account_expense_ids = company_props.with_context(
-            property_field='property_account_expense_id')._get_companies()
+    @api.depends()
+    def _compute_properties(self):
+        company_property = self.env['res.company.property']
+        property_fields = dict(
+            property_account_income_ids=
+            'property_account_income_id',
+            property_account_expense_ids=
+            'property_account_expense_id',
+        )
+        for product in self:
+            company_properties = company_property.with_context(
+                active_model='product.template',
+                active_id=self.id)
+            values = {}
+            for newfield, oldfield in property_fields.items():
+                values.update({
+                    newfield: company_properties.with_context(
+                        property_field=oldfield)._get_companies()
+                })
+            product.update(values)
 
     @api.multi
     def action_company_properties(self):
