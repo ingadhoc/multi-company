b35268c2dc77d08cb77768e24c82f128abf5acdd
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-05-31 20:34:49 -0300

    [MERGE] port-foward from 9.0 (#11)
    * [IMP] Fixed compatibility with sales types when generating invoice advances (#10)

--- sale_multic_fix/models/sale_order.py
+++ sale_multic_fix/models/sale_order.py
@@ -128,12 +128,15 @@ class SaleAdvancePaymentInv(models.TransientModel):
     @api.multi
     def _create_invoice(self, order, so_line, amount):
         self.ensure_one()
+        company_id = order.company_id.id
+        if 'type_id' in order._fields and order.type_id.journal_id:
+            company_id = order.type_id.journal_id.company_id.id
         order = order.with_context(
-            force_company=order.company_id.id)
+            force_company=company_id)
         self = self.with_context(
-            company_id=order.company_id.id,
-            default_company_id=order.company_id.id,
-            force_company=order.company_id.id)
+            company_id=company_id,
+            default_company_id=company_id,
+            force_company=company_id)
         invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
             order, so_line, amount)
         # este es el metodo que odoo usa en sale pero mas corrrecto seria
