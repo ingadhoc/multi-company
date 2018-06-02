e3cff7e14ef9c0dfa160cd3834dc7ba67b7fcfa4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-06-02 09:02:39 -0300

    [ADD] sale_stock_multic_fix module

---
+++ sale_stock_multic_fix/models/sale_order.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import models, api, _
+from openerp.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    @api.constrains('company_id', 'warehouse_id')
+    def check_company(self):
+        for rec in self:
+            warehouse_id_company = rec.warehouse_id.company_id
+            if warehouse_id_company and warehouse_id_company != rec.company_id:
+                raise ValidationError(_(
+                    'The warehouse company must be the same as the sale '
+                    'order company'))
