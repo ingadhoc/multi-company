586e4bbe7047f69012d077db7c08100f11a482cd
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-08-01 19:54:03 -0300

    [FIX] properties on m2m

--- account_multicompany_ux/models/product_template.py
+++ account_multicompany_ux/models/product_template.py
@@ -8,21 +8,14 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    def _get_property_context(property_field):
-        return (
-            "{'active_model': 'product.template', 'active_id': id, "
-            "'property_field': '%s'}" % property_field)
-
     property_account_income_ids = fields.Many2many(
         'res.company.property',
         string="Income Account",
-        context=_get_property_context('property_account_income_id'),
         compute='_compute_properties',
     )
     property_account_expense_ids = fields.Many2many(
         'res.company.property',
         string="Expense Account",
-        context=_get_property_context('property_account_expense_id'),
         compute='_compute_properties',
     )
 
@@ -30,10 +23,8 @@ class ProductTemplate(models.Model):
     def _compute_properties(self):
         company_property = self.env['res.company.property']
         property_fields = dict(
-            property_account_income_ids=
-            'property_account_income_id',
-            property_account_expense_ids=
-            'property_account_expense_id',
+            property_account_income_ids='property_account_income_id',
+            property_account_expense_ids='property_account_expense_id',
         )
         for product in self:
             company_properties = company_property.with_context(
--- account_multicompany_ux/models/res_partner.py
+++ account_multicompany_ux/models/res_partner.py
@@ -9,45 +9,34 @@ class ResPartner(models.Model):
 
     _inherit = 'res.partner'
 
-    def _get_property_context(property_field):
-        return (
-            "{'active_model': 'res.partner', 'active_id': id, "
-            "'property_field': '%s'}" % property_field)
-
     property_account_receivable_ids = fields.Many2many(
         'res.company.property',
         string="Account Receivable",
-        context=_get_property_context('property_account_receivable_id'),
         compute='_compute_properties',
     )
     property_account_payable_ids = fields.Many2many(
         'res.company.property',
         string="Account Payable",
-        context=_get_property_context('property_account_payable_id'),
         compute='_compute_properties',
     )
     property_account_position_ids = fields.Many2many(
         'res.company.property',
         string="Fiscal Position",
-        context=_get_property_context('property_account_position_id'),
         compute='_compute_properties',
     )
     property_payment_term_ids = fields.Many2many(
         'res.company.property',
         string='Customer Payment Term',
-        context=_get_property_context('property_payment_term_id'),
         compute='_compute_properties',
     )
     property_supplier_payment_term_ids = fields.Many2many(
         'res.company.property',
         string='Supplier Payment Term',
-        context=_get_property_context('property_supplier_payment_term_id'),
         compute='_compute_properties',
     )
     property_product_pricelist_ids = fields.Many2many(
         'res.company.property',
         string='Sale Pricelist',
-        context=_get_property_context('property_product_pricelist'),
         compute='_compute_properties',
     )
 
