28a90a9173cf9f07246966417cf24de33cebdbcc
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-08-17 16:59:32 -0300

    [FIX] multicompany_ux order on loading views

--- account_multicompany_ux/__manifest__.py
+++ account_multicompany_ux/__manifest__.py
@@ -32,6 +32,7 @@
         'views/res_partner_views.xml',
         'views/product_category_views.xml',
         'views/product_template_views.xml',
+        'views/product_product_views.xml',
         'views/account_journal_dashboard_views.xml',
         'wizards/res_config_settings_views.xml',
         'security/account_multicompany_ux_security.xml',
