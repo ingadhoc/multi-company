552329332476d927d1c1af57e4e29bb743aee880
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-11-06 11:57:15 -0300

    [IMP] some improvemnt when create invoice with avanced payment in different company (#26)

--- sale_multic_fix/wizards/sale_advance_payment_inv.py
+++ sale_multic_fix/wizards/sale_advance_payment_inv.py
@@ -10,6 +10,13 @@ class SaleAdvancePaymentInv(models.TransientModel):
 
     @api.multi
     def _create_invoice(self, order, so_line, amount):
+        """ Corregimos facturas de adelantos para dos casos:
+        1. Al estar logueados en una cia pero que el pedido sea de cia hija
+        2. al usar sale type con un journal de una cia hija
+        En el caso de que haya posicion fiscal seteada y con compañia, la
+        borramos momentaneamente porque el metodo de odoo "_create_invoice"
+        no es heredable (Como el _prepare_invoice).
+        """
         self.ensure_one()
         company_id = order.company_id.id
         if 'type_id' in order._fields and order.type_id.journal_id:
@@ -21,19 +28,16 @@ class SaleAdvancePaymentInv(models.TransientModel):
             company_id=company_id,
             default_company_id=company_id,
             force_company=company_id)
-
+        original_fiscal_position_id = False
+        if order.fiscal_position_id.company_id and order.\
+                fiscal_position_id.company_id.id != company_id:
+            original_fiscal_position_id = order.fiscal_position_id
+            order.fiscal_position_id = False
         invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
             order, so_line, amount)
-        # este es el metodo que odoo usa en sale pero mas corrrecto seria
-        # usar el get_journal
-        invoice.journal_id = self.env['account.invoice'].default_get(
-            ['journal_id'])['journal_id']
 
-        # fix taxes of advance product only for sale_order_type compatibility
-        # when company of journal is different from SO
-        if company_id != order.company_id.id:
-            for inv_line in invoice.invoice_line_ids:
-                inv_line._set_taxes()
-            invoice.compute_taxes()
-
+        if original_fiscal_position_id:
+            order.fiscal_position_id = original_fiscal_position_id
+        invoice._onchange_company()
+        invoice.compute_taxes()
         return invoice
