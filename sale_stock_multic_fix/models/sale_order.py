ff1d3d841ab13b4a6e40b1eb4fa17e8f3cc2b1df
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-02-18 22:25:47 -0300

    [REF] add comment

--- sale_stock_multic_fix/models/sale_order.py
+++ sale_stock_multic_fix/models/sale_order.py
@@ -11,6 +11,13 @@ class SaleOrder(models.Model):
 
     @api.constrains('company_id', 'warehouse_id')
     def check_company(self):
+        """ Esta constraint, además de arreglar esta inconsistencia de manera
+        voluntaria tambien arregla, por ejemplo, lo que si podría producir
+        si se configura website en cia hija y admin tiene cia padre, en ese
+        caso, desde ecommerce, no se encuentra warehouse pero se intenta
+        crear venta en cia hija y admin obtiene valor por defecto de warehouse
+        para cia padre
+        """
         for rec in self.filtered(
                 lambda s: s.warehouse_id and s.warehouse_id.company_id !=
                 s.company_id):
