2d5703043a9d0b42c44de6853a233375bab2a4e3
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-09-13 16:30:14 -0300

    [REL] Migration sale_stock_multic_fix to 12.0 (#46)
    * [FIX] sale_stock_multic_fix: Make instalable
    
    * [FIX] sale_stock_multic_fix: Change the openerp to manifest
    
    * [IMP] sale_stock_multic_fix: Use any to validate this constrains

--- sale_stock_multic_fix/__init__.py
+++ sale_stock_multic_fix/__init__.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
+# For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
 from . import models
