16db05f1476f503e523999cb15d01b891d17f2f6
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-09-13 15:26:55 -0300

    [IMP] purchase_multi_fix separe in purchase_stock_ multi_fix to incoporate the part related with puchase_stock

--- purchase_multic_fix/models/purchase_order.py
+++ purchase_multic_fix/models/purchase_order.py
@@ -2,20 +2,11 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, api, _
-from odoo.exceptions import ValidationError
+from odoo import models, api
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
-
-    @api.constrains('company_id', 'picking_type_id')
-    def check_company(self):
-        picking_type_company = self.picking_type_id.warehouse_id.company_id
-        if picking_type_company and picking_type_company != self.company_id:
-            raise ValidationError(_(
-                'The picking type company must be the same as the purchase '
-                'order company'))
 
     @api.onchange('partner_id', 'company_id')
     def onchange_partner_id(self):
