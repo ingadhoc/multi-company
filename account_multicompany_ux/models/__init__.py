50259506a1d1d02775f2413eb1326399d72b0b6c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-05-12 17:32:16 -0300

    [REM] account_multicompany_ux: name get from many models Realmente no es necesario ya que ahora se sta filtrando al elegir estos registros (cuentas, fiscal position, impuestos, etc). Solo dejamos la funcionalidad para los diarios ya que no hay una selección previa de la compañía

--- account_multicompany_ux/models/__init__.py
+++ account_multicompany_ux/models/__init__.py
@@ -4,12 +4,8 @@
 ##############################################################################
 from . import res_company
 from . import res_company_property
-from . import account_fiscal_position
 from . import account_move
-from . import account_analytic_account
-from . import account_account
 from . import account_journal
-from . import account_tax
 from . import res_partner
 from . import product_template
 from . import product_product
