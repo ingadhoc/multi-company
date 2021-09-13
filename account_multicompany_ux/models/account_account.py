33b88afbcf2dbb6a634f30aada747d0ed7d743fa
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-09-13 09:35:50 -0300

    [IMP] multicompany_ux: add suffix on tax and accounts Lo agregamos para dos casos de usos: * en productos, que se seleccionan impuestos para más de una compañía * en informes financieros que se puede visualizar info de más de una cia
    Además mejoramos el método que devuelve el suffix para que solo lo haga si hay más de una cia seleccionada

---
+++ account_multicompany_ux/models/account_account.py
@@ -0,0 +1,18 @@
+from odoo import api, models
+
+
+class AccountAccount(models.Model):
+
+    _inherit = 'account.account'
+
+    def name_get(self):
+        """
+        Agregamos sufijo de compañía en reportes financieros si hay más de una compañía seleccionada
+        """
+        res = super().name_get()
+        if self._context.get('model') == 'account.financial.html.report' and len(self._context.get('company_ids')) > 1:
+            new_res = []
+            for record, res in zip(self, res):
+                new_res.append((res[0], '%s%s' % (res[1], record.company_id.get_company_sufix())))
+            return new_res
+        return res
