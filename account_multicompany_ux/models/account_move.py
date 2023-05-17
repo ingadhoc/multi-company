7cf802dc1e93cf68670a222424e483991047bcef
Juan Jose Scarafia <jjs@adhoc.com.ar>
2023-05-17 17:14:36 -0300

    [REM] _multic_fix and merge into account_multicompany_ux
    Mucha de la logica de multic_fix la movimos a pr a odoo https://github.com/odoo/odoo/pull/121725
    Luego ese modulo ya no tiene sentido y en realidad no es un fix de nada.
    
    Movemos el permitir cambio entre diarios de distintas compañías a account_multicompany_ux

--- account_multicompany_ux/models/account_move.py
+++ account_multicompany_ux/models/account_move.py
@@ -15,3 +15,15 @@ class AccountMove(models.Model):
             if move.company_id.consolidation_company:
                 raise ValidationError(_(
                     'You can not create entries on a consolidation company'))
+
+    @api.depends('company_id', 'invoice_filter_type_domain')
+    def _compute_suitable_journal_ids(self):
+        """
+        We override this method to add filter by companies in the env instead of the company of the user
+        For this to work the pr is needed https://github.com/odoo/odoo/pull
+        """
+        for m in self:
+            journal_type = m.invoice_filter_type_domain or 'general'
+            company_ids = self.env.companies.ids
+            domain = [('company_id', 'in', company_ids), ('type', '=', journal_type)]
+            m.suitable_journal_ids = self.env['account.journal'].search(domain)
