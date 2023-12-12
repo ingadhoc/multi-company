7d243555a84b6fe44388b93c1baf30f838a3982f
Pablo Montenegro <pam@adhoc.com.ar>
2023-12-12 08:38:40 -0300

    [MIG] website_multicompany: Migration to 17.0

--- website_multicompany/__manifest__.py
+++ website_multicompany/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Website Multicompany',
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -28,6 +28,6 @@
     ],
     'data': [],
     'demo': [],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
 }
