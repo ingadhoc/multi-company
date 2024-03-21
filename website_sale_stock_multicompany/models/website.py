f4c2cd6a20f9a1c56daec252a3706ae301ba7af1
Martin Quinteros <maq@adhoc.com.ar>
2024-03-21 14:39:14 -0300

    [FIX] FIX _get method in ir.default

--- website_sale_stock_multicompany/models/website.py
+++ website_sale_stock_multicompany/models/website.py
@@ -14,7 +14,7 @@ class Website(models.Model):
         if self._context.get('company_fix'):
             company = self._context.get('company_fix')
             return (
-                        self.env['ir.default'].get('sale.order', 'warehouse_id', company_id=company) or
+                        self.env['ir.default'].sudo()._get('sale.order', 'warehouse_id', company_id=company) or
                         self.env['stock.warehouse'].sudo().search([('company_id', '=', company)], limit=1).id
                 )
         return super()._get_warehouse_available()
