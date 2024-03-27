4e7a41f1f0b31e3c77007def3ff44a2a869c4791
Pablo Montenegro <pam@adhoc.com.ar>
2024-03-27 12:41:33 -0300

    [WIP] account_multicompany_ux: agregar https://github.com/odoo/odoo/pull/121725 a este módulo Task: 34410

--- account_multicompany_ux/models/account_move.py
+++ account_multicompany_ux/models/account_move.py
@@ -18,3 +18,24 @@ class AccountMove(models.Model):
             if move.company_id.consolidation_company:
                 raise ValidationError(_(
                     'You can not create entries on a consolidation company'))
+
+    @api.depends('company_id')
+    def _compute_partner_bank_id(self):
+        """ If company is changed the partner bank must be updated."""
+        return super()._compute_partner_bank_id()
+
+    @api.depends('company_id')
+    def _compute_invoice_payment_term_id(self):
+        for move in self:
+            # avoid changing payment term if same partner and existing payment term is suitable for current company
+            if move.partner_id == move._origin.partner_id and (
+                    not move.invoice_payment_term_id.company_id or move.invoice_payment_term_id.company_id == move.company_id):
+                continue
+            move = move.with_company(move.company_id)
+            return super(AccountMove,move)._compute_invoice_payment_term_id()
+
+    @api.onchange('company_id')
+    def _onchange_company_id(self):
+        for move in self:
+            if not self._origin:
+                self.invoice_line_ids.company_id = self.company_id
