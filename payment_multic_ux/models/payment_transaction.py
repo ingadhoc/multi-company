35ee1f15c8d8e248d5b05aeb4a5a6e36614cf063
docker-odoo <awe@adhoc.com.ar>
2024-05-22 19:40:43 +0000

    [FIX] account_multic_ux: company context key

--- payment_multic_ux/models/payment_transaction.py
+++ payment_multic_ux/models/payment_transaction.py
@@ -6,4 +6,4 @@ class PaymentTransaction(models.Model):
 
     def _create_payment(self, **extra_create_values):
         # Add force_company to catch later in the account.payment creation.  
-        return super(PaymentTransaction, self.with_context(force_company_id = self.provider_id.journal_id.company_id.id))._create_payment(**extra_create_values)
+        return super(PaymentTransaction, self.with_company(self.provider_id.journal_id.company_id.id))._create_payment(**extra_create_values)
