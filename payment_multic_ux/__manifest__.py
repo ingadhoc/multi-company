60ae9e2ca885a29785e64b23dd70de5b72baf254
docker-odoo <docker-odoo@example.com>
2024-10-15 15:38:15 +0000

    [MIG] payment_multic_ux: Migration to 18.0

--- payment_multic_ux/__manifest__.py
+++ payment_multic_ux/__manifest__.py
@@ -19,15 +19,15 @@
 ##############################################################################
 {
     'name': 'Payment Multicompany Usability',
-    'version': "17.0.2.0.0",
+    'version': "18.0.1.0.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
     'depends': [
-        'account_payment'
+        'account_payment',
     ],
     'data': [],
     'demo': [],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
 }
