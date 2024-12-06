1f663b7ea76443f49464dc42ad366862bb5f0713
docker-odoo <docker-odoo@example.com>
2024-12-06 13:26:16 +0000

    [MIG] website_sale_multicompany: Migration to 18.0
    [MIG] website_sale_stock_multicompany: Migration to 18.0
    
    closes ingadhoc/multi-company#178
    
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- website_sale_multicompany/models/website.py
+++ website_sale_multicompany/models/website.py
@@ -12,11 +12,11 @@ class Website(models.Model):
 
     _inherit = 'website'
 
-    def sale_get_order(self, force_create=False, update_pricelist=False):
-        sale_order_sudo = super().sale_get_order(force_create, update_pricelist)
+    def sale_get_order(self, force_create=False):
+        sale_order_sudo = super().sale_get_order(force_create)
         # _sale_get_order_company_fix modifica los valores company related de la orden
         # Durante la creacion de la orden en el website la company siempre debe ser
-        # la del website por un Raise en el create(https://github.com/odoo/odoo/blob/16.0/addons/website_sale/models/sale_order.py#L38)
+        # la del website por un Raise en el create(https://github.com/odoo/odoo/blob/18.0/addons/website_sale/models/sale_order.py#L155)
         # por eso creo la orden y luego la modifico
         vals = sale_order_sudo._sale_get_order_company_fix()
         if vals:
