d58b8a5156d739a14bd097c1f275411b58e818e4
augusto-weiss <awe@adhoc.com.ar>
2025-07-25 14:26:39 +0000

    [FIX] website_sale_multic_ux: set company False for website
    closes ingadhoc/multi-company#195
    
    Signed-off-by: Juan Ignacio Rivero <jr@adhoc.com.ar>

--- website_sale_multic_ux/controllers/main.py
+++ website_sale_multic_ux/controllers/main.py
@@ -4,5 +4,5 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSale(WebsiteSale):
     def _complete_address_values(self, address_values, address_type, use_delivery_as_billing, order_sudo):
         super()._complete_address_values(address_values, address_type, use_delivery_as_billing, order_sudo)
-        if not address_values["user_id"]:
+        if order_sudo._is_anonymous_cart():
             address_values["company_id"] = False
