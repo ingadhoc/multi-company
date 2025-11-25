be8d296b065fdd6fcc57e3280fd440618765dfc8
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-11-25 15:04:06 +0000

    [MIG] website_sale_stock_multicompany: Migration to 19.0

--- website_sale_stock_multicompany/models/website.py
+++ website_sale_stock_multicompany/models/website.py
@@ -10,8 +10,8 @@ class Website(models.Model):
     _inherit = "website"
 
     def _get_warehouse_available(self):
-        if self._context.get("company_fix"):
-            company = self._context.get("company_fix")
+        if self.env.context.get("company_fix"):
+            company = self.env.context.get("company_fix")
             return (
                 self.env["ir.default"].sudo()._get("sale.order", "warehouse_id", company_id=company)
                 or self.env["stock.warehouse"].sudo().search([("company_id", "=", company)], limit=1).id
