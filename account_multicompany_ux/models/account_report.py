f79cb7caed0472c3482626233d8f6dbc3eb2fadc
Virginia <vib@adhoc.inc>
2025-12-11 18:58:44 -0300

    Update project.toml from template

--- account_multicompany_ux/models/account_report.py
+++ account_multicompany_ux/models/account_report.py
@@ -19,7 +19,13 @@ class AccountReport(models.Model):
         horizontal_split_side,
         unfold_all_batch_data=None,
     ):
-        """Agregamos sufijo de compañía en reportes financieros si hay más de una compañía seleccionada en el método _compute_display_name de account.account pero necesitamos agregar por contexto el id del reporte porque si no lo hacemos al momento de hacer un unfold de algún rubro en el reporte las cuentas se muestran sin dicho sufijo. Ver ticket 43453 para ver más info acerca de la funcionalidad que necesitamos."""
+        """Agregamos sufijo de compañía en reportes financieros si hay más de
+        una compañía seleccionada en el método _compute_display_name de
+        account.account pero necesitamos agregar por contexto el id del reporte
+        porque si no lo hacemos al momento de hacer un unfold de algún rubro
+        en el reporte las cuentas se muestran sin dicho sufijo. Ver ticket
+        43453 para ver más info acerca de la funcionalidad que necesitamos.
+        """
         self = self.with_context(report_id=self.id)
 
         return super()._expand_unfoldable_line(
