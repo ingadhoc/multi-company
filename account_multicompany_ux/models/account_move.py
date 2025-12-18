e8033bd292efa9c63fbc25bbea4110f43130c2af
Rocío Vega <rov@adhoc.com.ar>
2025-12-18 13:07:42 -0300

    [IMP] account_multicompany_ux: add company_can_change field and update button visibility logic

--- account_multicompany_ux/models/account_move.py
+++ account_multicompany_ux/models/account_move.py
@@ -11,6 +11,17 @@ class AccountMove(models.Model):
 
     company_id = fields.Many2one(tracking=True)
 
+    company_can_change = fields.Boolean(
+        compute="_compute_company_can_change",
+        help="True if the company has no branches and is not a branch",
+    )
+
+    @api.depends("company_id", "company_id.parent_id", "company_id.child_ids")
+    def _compute_company_can_change(self):
+        for move in self:
+            # Can change company if the company has no branches and is not a branch
+            move.company_can_change = move.company_id.parent_id or move.company_id.child_ids
+
     @api.constrains("company_id")
     def check_company(self):
         for move in self:
