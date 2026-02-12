0747020d14c78860373a18474c9b8fb27d97c8a2
roboadhoc <roboadhoc@adhoc.com.ar>
2026-02-12 13:57:59 +0000

    [BOT] Bump version: account_multicompany_ux 18.0.1.7.0
    Merged: ingadhoc/multi-company#245

--- account_multicompany_ux/__manifest__.py
+++ account_multicompany_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Account Multicompany Usability",
-    "version": "18.0.1.6.0",
+    "version": "18.0.1.7.0",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
