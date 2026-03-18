481b63ebe56d6070002e58fbeb4648fa9a3998d0
Rocío Vega <rov@adhoc.com.ar>
2026-03-18 08:58:47 -0300

    [FIX] account_multicompany_ux: fix test_account_multicompany_ux_unit_test
    closes ingadhoc/multi-company#268
    
    Signed-off-by: Camila Vives <cav@adhoc.inc>

--- account_multicompany_ux/tests/test_account_multicompany_ux_unit_test.py
+++ account_multicompany_ux/tests/test_account_multicompany_ux_unit_test.py
@@ -44,7 +44,8 @@ class TestAccountMulticompanyUxUnitTest(common.TransactionCase):
         self.bank_1 = self.env['res.partner.bank'].search([('company_id','=', False)], limit=1)
         self.bank_1.write({'partner_id': self.first_company.partner_id.id})
         self.bank_1.company_id = self.first_company.id
-        self.bank_1.allow_out_payment = True
+        # In tests, trusting bank accounts requires install_mode/test_enable even with sudo.
+        self.bank_1.with_context(install_mode=True).sudo().allow_out_payment = True
         self.env.company = self.first_company
 
     def test_multicompany_sale_order(self):
@@ -79,7 +80,9 @@ class TestAccountMulticompanyUxUnitTest(common.TransactionCase):
             'company_id': self.first_company.id,
             'journal_id': self.first_company_journal.id
         })
+        self.bank_1.with_context(install_mode=True).sudo().allow_out_payment = False
         self.bank_1.company_id = False
+        self.bank_1.with_context(install_mode=True).sudo().allow_out_payment = True
         invoice.write({"partner_bank_id": self.bank_1.id})
 
         acc.change_company()
