1a004bf230d1d470cda97d4cedf194f501929008
Franco Leyes <lef@adhoc.com.ar>
2026-04-06 14:06:06 +0000

    [FIX] account_multicompany_ux: Improve validation for access to related sale orders in account.move and account.move.line

--- account_multicompany_ux/models/account_move.py
+++ account_multicompany_ux/models/account_move.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -16,4 +16,14 @@ class AccountMove(models.Model):
     def check_company(self):
         for move in self:
             if move.company_id.consolidation_company:
-                raise ValidationError(_("You can not create entries on a consolidation company"))
+                raise ValidationError(self.env._("You can not create entries on a consolidation company"))
+
+    def _reverse_moves(self, default_values_list=None, cancel=False):
+        sale_lines = self.env["sale.order.line"].sudo().search([("invoice_lines.move_id", "in", self.ids)])
+        if sale_lines and not sale_lines.with_user(self.env.user).has_access("write"):
+            raise ValidationError(
+                self.env._(
+                    "You cannot create a credit note for invoices related to sale orders you do not have access to."
+                )
+            )
+        return super()._reverse_moves(default_values_list=default_values_list, cancel=cancel)
