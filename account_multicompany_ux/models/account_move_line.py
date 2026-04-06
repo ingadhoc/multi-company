37eae1a8f77baa943bad13f7bdba42cd02e8e009
Franco Leyes <lef@adhoc.com.ar>
2026-04-06 14:06:06 +0000

    [FIX] account_multicompany_ux: Improve validation for access to related sale orders in account.move and account.move.line
    closes ingadhoc/multi-company#276
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- account_multicompany_ux/models/account_move_line.py
+++ account_multicompany_ux/models/account_move_line.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import _, api, models
+from odoo import api, models
 from odoo.exceptions import ValidationError
 
 
@@ -17,5 +17,18 @@ class AccountMoveLine(models.Model):
         sale_lines = self.env["sale.order.line"].sudo().search([("invoice_lines.move_id", "in", move_ids)])
         if sale_lines and not sale_lines.with_user(self.env.user).has_access("write"):
             raise ValidationError(
-                _("No puede eliminar lineas de factura relacionadas con ordenes de venta a las que no tiene acceso.")
+                self.env._("You cannot delete invoice lines related to sale orders you do not have access to.")
             )
+
+    def write(self, vals):
+        if "quantity" in vals:
+            draft_lines = self.filtered(lambda l: l.move_id.state == "draft")
+            if draft_lines:
+                sale_lines = self.env["sale.order.line"].sudo().search([("invoice_lines", "in", draft_lines.ids)])
+                if sale_lines and not sale_lines.with_user(self.env.user).has_access("write"):
+                    raise ValidationError(
+                        self.env._(
+                            "You cannot change the quantity of invoice lines related to sale orders you do not have access to."
+                        )
+                    )
+        return super().write(vals)
