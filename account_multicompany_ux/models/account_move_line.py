9068111923e1784f589b32b7edb3ff57cc625135
Franco Leyes <lef@adhoc.com.ar>
2026-04-17 19:44:20 +0000

    [FIX] account_multicompany_ux: Enhance access control checks for invoice and invoice line operations
    closes ingadhoc/multi-company#279
    
    X-original-commit: 835101c7fbb45ab19269e6d57d3585980bf9a7df
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>
    Signed-off-by: Franco Leyes <lef@adhoc.com.ar>

--- account_multicompany_ux/models/account_move_line.py
+++ account_multicompany_ux/models/account_move_line.py
@@ -15,7 +15,7 @@ class AccountMoveLine(models.Model):
         if not move_ids:
             return self.env["sale.order.line"]
         sale_lines = self.env["sale.order.line"].sudo().search([("invoice_lines.move_id", "in", move_ids)])
-        if sale_lines and not sale_lines.with_user(self.env.user).has_access("write"):
+        if sale_lines and not (self.env.su or sale_lines.with_user(self.env.user).has_access("write")):
             raise ValidationError(
                 self.env._("You cannot delete invoice lines related to sale orders you do not have access to.")
             )
@@ -25,7 +25,7 @@ class AccountMoveLine(models.Model):
             draft_lines = self.filtered(lambda l: l.move_id.state == "draft")
             if draft_lines:
                 sale_lines = self.env["sale.order.line"].sudo().search([("invoice_lines", "in", draft_lines.ids)])
-                if sale_lines and not sale_lines.with_user(self.env.user).has_access("write"):
+                if sale_lines and not (self.env.su or sale_lines.with_user(self.env.user).has_access("write")):
                     raise ValidationError(
                         self.env._(
                             "You cannot change the quantity of invoice lines related to sale orders you do not have access to."
